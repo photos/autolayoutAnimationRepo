598ca918c5bcd4514503636523d8640667c10a7a
photos <myjellymail@gmail.com>
2015-12-03 09:33:24 -0800

    Initial Commit

---
+++ autolayoutAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autolayoutAnimation
+//
+//  Created by Forrest Collins on 12/3/15.
+//  Copyright © 2015 Forrest Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
